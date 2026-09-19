a3d3c69152ec0939d48eaa84dda969b9705ed115
Taylor Inman <taylor.inman.ctc@outlook.com>
2026-09-19 13:54:22 -0700

    uploaded files/folders

---
+++ DM_IG/Javascript/Engine/eventManager.js
@@ -0,0 +1,34 @@
+let settingsButtonID = document.getElementById("settings_button");
+let researchButtonID = document.getElementById("research_button");
+let productionButtonID = document.getElementById("production_button");
+let upgradesButtonID = document.getElementById("upgrades_button");
+let managersButtonID = document.getElementById("managers_button");
+
+function initListeners() {
+	
+	// Change view to Production View
+	productionButtonID.addEventListener("click", function () {
+		alert("Production!");
+	});
+
+	// Change view to Research View 
+	researchButtonID.addEventListener("click", function () {
+		alert("Research!");
+	});
+	
+	// Change view to Upgrades View 
+	upgradesButtonID.addEventListener("click", function () {
+		alert("Upgrades!");
+	});
+	
+	// Change view to Managers View
+	managersButtonID.addEventListener("click", function () {
+		alert("Managers!");
+	});
+	
+	// Change view to Settings View
+	settingsButtonID.addEventListener("click", function () {
+		alert("Settings!");
+	});
+}
+
---
+++ DM_IG/Javascript/main.js
@@ -0,0 +1,12 @@
+const floaters = ["Hello World!", "Nobody here but us chickens!", "I wish I wish I was a fish!", "BYOB(Bring Your Own Bits)", "Why was six afraid of seven? Because seven ate nine!", "A long time ago, in a galaxy far, far away...", "Knock, knock!", "Orange you glad I didn't say banana?", "Banana!"];
+
+function init() {
+	i = Math.round(Math.random() * floaters.length);
+	document.getElementById("float_bar").innerHTML = floaters[i];
+	
+	initListeners();
+	initBuildings();
+	
+	setInterval(tickTock, 100);
+	setInterval(drift, 15000);
+}
