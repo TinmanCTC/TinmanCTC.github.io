18fe7969c1ce9bfdc5db834124dbd407bd7b6a46
Taylor Inman <taylor.inman.ctc@outlook.com>
2026-09-19 18:51:41 -0700

    test

--- DM_IG/Javascript/main.js
+++ DM_IG/Javascript/main.js
@@ -8,7 +8,7 @@ layout: page
 const floaters = ["Hello World!", "Nobody here but us chickens!", "I wish I wish I was a fish!", "BYOB(Bring Your Own Bits)", "Why was six afraid of seven? Because seven ate nine!", "A long time ago, in a galaxy far, far away...", "Knock, knock!", "Orange you glad I didn't say banana?", "Banana!"];
 
 function init() {	
-	import data from "/DM_IG/Javascript/Data/Buildings.json" with: { type: "json" };
+	
 	
 	i = Math.round(Math.random() * floaters.length);
 	document.getElementById("float_bar").innerHTML = floaters[i];
