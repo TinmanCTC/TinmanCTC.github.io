1a952179b485ce4fe779163bd99cd8bc0b84dec7
Taylor Inman <taylor.inman.ctc@outlook.com>
2026-09-19 18:29:55 -0700

    Update Buildings.js

--- DM_IG/Javascript/Subsystems/Buildings.js
+++ DM_IG/Javascript/Subsystems/Buildings.js
@@ -11,8 +11,8 @@ layout: page
  * Utilized by Subsystems/Buildings.js
  * 
  */
-const data = import("/DM_IG/Javascript/Data/Buildings.json", { with: { type: "json" } });
-const bldg = JSON.parse(data);
+//const data = import("/DM_IG/Javascript/Data/Buildings.json", { with: { type: "json" } });
+const bldg = JSON.parse("/DM_IG/Javascript/Data/Buildings.json");
 
 function Building(id, name, unlocked, baseOutput, baseBonus, mngCost, mngInterval, powerUse, clickInterval, imgStr) {
 	this.id = id;
