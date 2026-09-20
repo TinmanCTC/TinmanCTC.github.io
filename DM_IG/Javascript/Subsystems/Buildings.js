6954e7af2b371c33e9e5eaea29e9e34716592717
Taylor Inman <taylor.inman.ctc@outlook.com>
2026-09-19 17:01:56 -0700

    test

--- DM_IG/Javascript/Subsystems/Buildings.js
+++ DM_IG/Javascript/Subsystems/Buildings.js
@@ -12,8 +12,100 @@ layout: page
  * 
  */
 
-let Punchcard = [];
-let Desktop = [];
+const Building = {
+	Punchcard: {
+		id: "punchcard",
+		name: "Punch Card Computer",
+		startUnlocked: true,
+		baseOutput: 864,
+		baseBonus: 0,
+		baseCost: 50,
+		managerCost: 5000,
+		powerUse: 100,
+		clickInterval: 100,
+		managerInterval: 500,
+		imgStr: "Assets/Images/punchcard_framed.png"
+	},
+	Desktop: {
+		id: "desktop",
+		name: "Desktop Computer",
+		startUnlocked: false,
+		baseOutput: 2056,
+		baseBonus: 0,
+		baseCost: 1000,
+		managerCost: 5000,
+		powerUse: 250,
+		clickInterval: 2000,
+		managerInterval: 7500,
+		imgStr: "desktop_img"
+	},
+	Cubicle: {
+		id: "cubicle",
+		name: "Cubicle Workstation",
+		startUnlocked: false,
+		baseOutput: 32568,
+		baseBonus: 0,
+		baseCost: 3500,
+		managerCost:25000,
+		powerUse: 500,
+		clickInterval: 5000,
+		managerInterval: 10000,
+		imgStr: "cubicle_img"
+	},
+	LAN: {
+		id: "lan",
+		name: "LAN Server",
+		startUnlocked: false,
+		baseOutput: 1024000,
+		baseBonus: 0,
+		baseCost: 7500,
+		managerCost: 40000,
+		powerUse: 2000,
+		clickInterval:  10000,
+		managerInterval: 15000,
+		imgStr: "lan_img"
+	},
+	Cheap_Gen: {
+		id: "cheapGen",
+		name: "Cheap Generator",
+		startUnlocked: false,
+		baseOutput: 10000,
+		baseBonus: 0,
+		baseCost: 15000,
+		powerUse: 10000,
+		clickInterval: 30000,
+		imgStr: "cheap_gen_img"
+	},
+	Cloud: {
+		id: "cloud",
+		name: "Cloud Server",
+		startUnlocked: false,
+		baseOutput: 10240000,
+		baseBonus: 0,
+		baseCost: 25000,
+		managerCost: 60000,
+		powerUse: 4000,
+		clickInterval: 30000,
+		managerInterval: 15000,
+		imgStr: "cloud_img"
+	},
+	Blockchain: {
+		id: "blockchain",
+		name: "Blockchain Processor",
+		startUnlocked: false,
+		baseOutput: 5120000,
+		baseBonus: 0.05,
+		baseCost: 100000,
+		managerCost: 100000,
+		powerUse: 10000,
+		clickInterval: 10000,
+		managerInterval: 15000,
+		imgStr: "blockchain_img"
+	}
+};
+
+let bldg1 = [];
+let bldg2 = [];
 
 class Buildings {
 	constructor(id, name,level = 0,quantity = 1,unlocked = false,baseProduction,baseCost,baseBonus = 0,powerUse,isClicked = false,timeClicked = 0,clickInterval,isManaged = false,mngCost = 500,mngInterval = 5000,imgString) {
@@ -64,8 +156,7 @@ class Buildings {
  
  function initBuildings() {
 	
-	
-	Punchcard = new Buildings(
+	bldg1 = new Buildings(
 		Building.Punchcard.id,
 		Building.Punchcard.name,
 		1,
